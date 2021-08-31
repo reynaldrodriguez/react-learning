6d669a9b80c256662a955a8b9512486529d12d7f
Reynald Rodriguez <rrodriguez@aldereca.com>
2021-08-31 14:28:36 -0400

    Conditional returns

--- react-theory/react-state-events/code/09-finished/src/components/Expenses/Expenses.js
+++ react-theory/react-state-events/code/09-finished/src/components/Expenses/Expenses.js
@@ -1,37 +1,27 @@
 import React, { useState } from 'react';
-
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
 
-  const filteredArray = props.items.filter((expense) => expense.date.getFullYear() === parseInt(filteredYear)
+  const filteredArray = props.items.filter((expense) => expense.date.getFullYear().toString() === filteredYear
   );
 
-  const [items, setItems] = useState(filteredArray)
+  //const [items, setItems] = useState(filteredArray)
   
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
-    setItems(filteredArray)
+    //setItems(filteredArray)
   };
 
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {
-          items.map((expense)=> (
-            <ExpenseItem
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-              key={expense.id}
-            />
-          ))
-        }
+        <ExpensesList items={filteredArray}/>
       </Card>
     </div>
   );
